3dfe0221b0917db33af8d3db45c6f6aab20d7228
zekokh <heroin160892>
2015-09-02 13:01:45 +0300

    change serer info

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,7 +17,7 @@ server '95.213.199.234', user: 'deploy', roles: %w{web appca}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
