1dfd5c878ab4dfd4dce0edb3dc957746e1aa2f27
zekokh <heroin160892>
2015-09-02 12:58:49 +0300

    change roles

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,9 +7,9 @@ server '95.213.199.234', user: 'deploy', roles: %w{web appca}
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+# role :app, %w{deploy@example.com}
+# role :web, %w{deploy@example.com}
+# role :db,  %w{deploy@example.com}
 
 # Extended Server Syntax
 # ======================
